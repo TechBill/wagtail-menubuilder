5282cc2f8eeb0935426def1644f4546603287b25
Bill <techbill@Bills-MacBook-Pro.local>
2025-01-21 11:48:35 -0600

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='wagtail-menubuilder',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
